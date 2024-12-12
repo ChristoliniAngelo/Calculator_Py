e3074c245051be0ea70d3c1f6074a834e7191d53
Christolini Angelo <Christoliniangelo@gmail.com>
2024-12-12 10:37:40 +0700

    fix multipication error

--- App.py
+++ App.py
@@ -43,7 +43,7 @@ entry_display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 #buttons mapping
 buttons = [
     ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("/", 1, 3),
-    ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("X", 2, 3),
+    ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("*", 2, 3),
     ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("-", 3, 3),
     ("0", 4, 0), (".", 4, 1), ("=", 4, 2), ("+", 4, 3),
     ("C", 5, 0)
@@ -63,6 +63,6 @@ for j in range(4):
 print("Hello There ^v^")
 import time
 time.sleep(1)
-print("Program is lauching....")
+print("Calculator is lauching....")
 time.sleep(1)
 root.mainloop()
